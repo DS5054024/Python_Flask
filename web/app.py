cb4a0a34d6eab36c573997fba25e72b66c0b6c38
DS5054024 <50883737+DS5054024@users.noreply.github.com>
2021-05-05 10:14:57 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,16 +1,28 @@
-#Import the flask module
-from flask import import Flask
-
-#Create a Flask constructor. It takes name of the current module as the argument
-app = Flask(__name__)
-
-#Create a route decorator to tell the application, which URL should be called for the #described function and define the function
-
-@app.route('/')
-def python_flask():
-return "Welcome to python with flask"
-
-#Create the main driver function
+import os
+2
+from flask import Flask, render_template, abort, url_for, json, jsonify
+3
+import json
+4
+5
+app = Flask(__name__,template_folder='.')
+6
+7
+# read file
+8
+with open('file.json', 'r') as myfile:
+9
+    data = myfile.read()
+10
+11
+@app.route("/")
+12
+def index():
+13
+    return render_template('index.html', title="page", jsonfile=json.dumps(data))
+14
+15
+16
 if __name__ == '__main__':
-#call the run method
-app.run(debug=True,host='0.0.0.0')
+17
+    app.run(debug=True, host='0.0.0.0')
